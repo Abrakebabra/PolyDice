5c2b52e2a686005c45086a6c254841f7468c05a2
Keith Lee <lee.mingkeith@gmail.com>
2020-07-01 02:33:29 +0900

    Initial Commit

---
+++ PolyDice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PolyDice
+//
+//  Created by Keith Lee on 2020/07/01.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
